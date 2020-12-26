bb62f638e8b73ea91ed92ed5ff11f6f1edba9edb
frecklescherokee <71302040+frecklescherokee@users.noreply.github.com>
2020-12-26 17:31:01 -0600

    made the program start, connect to DB and print out company name and program name

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const mysql = require("mysql2");
 const cTable = require("console.table");
 const inquirer = require("inquirer");
 
+// make arrays to hold the various questions we need for this app
+const departmentsArr = [];
+const employeesArr = [];
+const rolesArr = [];
+
+// ENTER THE NAME OF YOUR COMPANY
+let company_name = 'SAMSUNG AUSTIN SEMICONDUCTOR';
+
 
 // Create a connection to the DB
 const con = mysql.createConnection({
@@ -15,8 +23,23 @@ const con = mysql.createConnection({
     database: "company"
   });
 
+// connect to the DB
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    // console.log("Connected!");
 });
 
+// print the name of the program
+let whenConnected = (res, rej) => {
+    // call the SQL queries for the various tables' data and pushes it into the arrays declared above
+    queryArrays();
+    
+    // send a response back to the promise
+    res("yay!");
+
+    // print out the company name and 'employee tracker'
+    console.log ("");
+    console.log (company_name);
+    console.log ("employee tracker");
+    console.log ("");
+};
